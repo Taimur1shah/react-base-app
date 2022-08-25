1007c2a147a329343103f6360965e30ebc5c2e38
taimur-shah <bitrtpl@123>
2022-08-25 22:56:43 +0500

    minor change

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function Hi() {
+  return <div>Hello World! updated</div>;
+}
+
+ReactDOM.render(<Hi/>, document.querySelector('#root'));
+
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -15,3 +22,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
