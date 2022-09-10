5f78f063e71248ddc6dded89879fbe52543e881c
taimur-shah <bitrtpl@123>
2022-09-11 02:41:37 +0500

    new function or components add

---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './Hello.css';
+
+
+
+function Hello() {
+  return <p class="myname">This is para in hello woeld document</p>
+}
+
+
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 
-/*
-function Hi() {
-  return <div>Hello World! updated</div>;
-}*/
-
-ReactDOM.render(<div>Hello World! updated</div>, 
-document.getElementById('root'));
-
-/*
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
+  <div>Hello world from taimur shah 
+    <div><App name="my app tag" age="30" /></div>
+    <div><Hello /></div>
+  </div>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-*/
