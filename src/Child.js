9660e3316ce48fec759ffd3f0b15a77c50e7efb5
taimur-shah <bitrtpl@123>
2022-09-16 03:05:50 +0500

    add parent to child message

---
+++ src/Child.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+
+
+function Child(props) {
+  return <div >Child number {props.num} 
+  </div>
+}
+
+export default Child;
---
+++ src/Parent.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import Child from './Child'
+
+
+function Parent(props) {
+  return <div > Parent 
+    <Child num={props.num}></Child>
+  </div>
+}
+
+export default Parent;
